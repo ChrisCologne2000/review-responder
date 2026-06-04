bdb9c4d36e4117946b4c7de8089b0c0c0c047de7
Chris <ossowski2012@googlemail.com>
2026-06-04 13:27:33 +0200

    add full pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,9 +1,6 @@
+"use client"
+import PricingPage from '@/components/PricingPage'
+
 export default function Pricing() {
-  return (
-    <div style={{padding: 40}}>
-      <h1>ReviewPilot Pricing</h1>
-      <p>Starter: €59/Monat</p>
-      <p>Pro: €99/Monat</p>
-    </div>
-  )
+  return <PricingPage />
 }
