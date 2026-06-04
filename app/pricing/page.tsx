3945390c064af9cecca1c9bf63f9f3ed74cfe883
Chris <ossowski2012@googlemail.com>
2026-06-04 12:10:55 +0200

    test pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,9 @@
-"use client"
-import PricingPage from '@/components/PricingPage'
-
 export default function Pricing() {
-  return <PricingPage />
+  return (
+    <div style={{padding: 40}}>
+      <h1>ReviewPilot Pricing</h1>
+      <p>Starter: €59/Monat</p>
+      <p>Pro: €99/Monat</p>
+    </div>
+  )
 }
